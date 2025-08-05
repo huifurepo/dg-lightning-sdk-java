b12ef17e68691aacfa086f3bcf6aaee99118cb92
bo.dong <bo.dong@huifu.com>
2025-08-05 09:45:05 +0800

    test

--- src/test/java/com/huifu/dg/lightning/biz/DemoTest2.java
+++ src/test/java/com/huifu/dg/lightning/biz/DemoTest2.java
@@ -34,13 +34,13 @@ public class DemoTest2 {
         request.setTradeType("A_NATIVE");
         // 交易金额
         request.setTransAmt("0.10");
-        Map<String, Object> response = Factory.Payment.Common()
-                .optional("seller","zhangsan")
-                .optional("wx_data",getWxData()).create(request);
+        Map<String, Object> response = Factory.Payment.Common().create(request);
         ObjectMapper objectMapper = JacksonUtils.getInstance();
         System.out.println("返回数据:" +objectMapper.writeValueAsString(response) );
 
     }
+
+
 
     public static void test2() throws Exception{
         BasePay.initWithMerConfig(OppsMerchantConfigDemo.getMerchantConfig());
@@ -88,6 +88,30 @@ public class DemoTest2 {
 
     }
 
+
+    public static void test4() throws Exception{
+        BasePay.initWithMerConfig(OppsMerchantConfigDemo.getMerchantConfig());
+        PaymentRequest request = new PaymentRequest();
+        // 请求日期
+        request.setReqDate(DateTools.getCurrentDateYYYYMMDD());
+        // 请求流水号
+        request.setReqSeqId(SequenceTools.getReqSeqId32());
+        // 商户号
+        request.setHuifuId("6666000109133323");
+        // 商品描述
+        request.setGoodsDesc("hibs自动化-通用版验证");
+        // 交易类型
+        request.setTradeType("A_NATIVE");
+        // 交易金额
+        request.setTransAmt("0.10");
+        Map<String, Object> response = Factory.Payment.Common()
+                .optional("seller","zhangsan")
+                .optional("wx_data",getWxData()).create(request);
+        ObjectMapper objectMapper = JacksonUtils.getInstance();
+        System.out.println("返回数据:" +objectMapper.writeValueAsString(response) );
+
+    }
+
     private static String getWxData() throws Exception {
         ObjectMapper objectMapper = JacksonUtils.getInstance();
         ObjectNode objectNode = objectMapper.createObjectNode();
@@ -104,6 +128,7 @@ public class DemoTest2 {
         test();
         test2();
         test3();
+        test4();
     }
 
 }
